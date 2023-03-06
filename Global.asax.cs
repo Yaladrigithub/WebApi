59d5d723733fb39df6708eff65608e510b69f525
Yaladrigithub <125118814+Yaladrigithub@users.noreply.github.com>
2023-03-06 23:49:03 +0530

    Update Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -18,7 +18,7 @@ namespace WebApiproject1
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+           // GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
         }
     }
 }
